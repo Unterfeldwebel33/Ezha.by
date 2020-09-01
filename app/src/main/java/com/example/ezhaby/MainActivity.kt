7a6203f195ac2a2e7231c760600d747338a5c434
Peter Barkun <peterbarkun@gmail.com>
2020-09-01 14:12:37 +0300

    First Commit - Intro Screen

---
+++ app/src/main/java/com/example/ezhaby/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.ezhaby
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val introSliderAdapter = IntroSliderAdapter(
+        listOf(
+            IntroSlide(
+                "Долго ждешь свою очередь?",
+            "Сделай заказ любимых блюд по пути\n" +
+                    "к фудкорту из кафе и ресторанов,\n" +
+                    "оплатив его прямо здесь",
+                R.drawable.sp1
+            ),
+            IntroSlide(
+                "Приходи и забирай!",
+                "Тебе придет уведомление о готовности твоего заказа, после чего покажи QR-код сотруднику ресторана и можешь\n" +
+                        "забирать его",
+                R.drawable.sp2
+            ),
+            IntroSlide(
+                "Используй акционные купоны и скидки",
+                "Помимо купонов которые дают тебе кафе,\n" +
+                        "ты можешь использовать и наши ежемесячные купоны",
+                R.drawable.sp3
+            )
+        )
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        introSliderViewPager.adapter = introSliderAdapter
+        setupIndicators()
+        setCurrentIndicator(0)
+    }
+
+    private fun setupIndicators() {
+        val indicators = arrayOfNulls<ImageView>(introSliderAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(8,0,8,0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i].apply {
+                this?.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inacticve
+                    )
+                )
+                this?.layoutParams = layoutParams
+            }
+            indicatorsContainer.addView(indicators[i])
+        }
+    }
+
+    private fun setCurrentIndicator(index: Int){
+        val childCount = indicatorsContainer.childCount
+        for (i in 0 until childCount) {
+            val imageView = indicatorsContainer[i] as ImageView
+            if (i == index) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active
+                    )
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inacticve
+                    )
+                )
+            }
+        }
+    }
+}
