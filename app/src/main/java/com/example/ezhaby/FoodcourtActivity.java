1fb5e8ec34af540ec4f2bb1b253811f95b176957
Peter Barkun <peterbarkun@gmail.com>
2020-09-03 13:52:54 +0300

    1.0.5.2 Выезжает боковая менюха

--- app/src/main/java/com/example/ezhaby/FoodcourtActivity.java
+++ app/src/main/java/com/example/ezhaby/FoodcourtActivity.java
@@ -1,11 +1,14 @@
 package com.example.ezhaby;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.ezhaby.Helper.Classes.Home.Adapter.SortAdapter;
 import com.example.ezhaby.Helper.Classes.Home.Adapter.SortHelperClass;
@@ -18,6 +21,7 @@ public class FoodcourtActivity extends AppCompatActivity {
     //Верхняя карусель сортировки
     RecyclerView sortRecycler;
     RecyclerView.Adapter adapter;
+    ImageView menuIcon;
 
     //Боковое меню
     DrawerLayout drawerLayout;
@@ -32,13 +36,39 @@ public class FoodcourtActivity extends AppCompatActivity {
         sortRecycler = findViewById(R.id.sortRecycler);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
+        menuIcon = findViewById(R.id.menu_icon);
 
         //Вызов функций
         sortRecycler();
+        navigationDrawer();
+    }
+
+    //отрисовка появления меню
+    private void navigationDrawer() {
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
 
 
     }
 
+    //Закрытие меню по кнопке назад
+    @Override
+    public void onBackPressed(){
+
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else
+            super.onBackPressed();
+    }
+
+    //отрисовка карусели сортировки
     private void sortRecycler() {
         sortRecycler.setHasFixedSize(true);
         sortRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
