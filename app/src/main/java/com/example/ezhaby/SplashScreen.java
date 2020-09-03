5fb0965f866a77acd2d3985cf240e47c7f274beb
Peter Barkun <peterbarkun@gmail.com>
2020-09-03 15:06:40 +0300

    1.0.6 Криво, но работает SplashScreen

---
+++ app/src/main/java/com/example/ezhaby/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.example.ezhaby;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends Activity {
+
+    private static int SPLASH_TIMER = 1000;
+
+    ImageView backgroundImage;
+    TextView poweredByLine;
+
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        backgroundImage = findViewById(R.id.background_image);
+        poweredByLine = findViewById(R.id.poweredBy);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMER);
+
+    }
+}
