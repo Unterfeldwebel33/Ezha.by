f1766d8f315c544645fe72acba8c42fff2e0de72
Peter Barkun <peterbarkun@gmail.com>
2020-09-03 12:06:00 +0300

    1.0.5.1 Выезжает боковая менюха через драг

--- app/src/main/java/com/example/ezhaby/FoodcourtActivity.java
+++ app/src/main/java/com/example/ezhaby/FoodcourtActivity.java
@@ -1,6 +1,7 @@
 package com.example.ezhaby;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,22 +9,34 @@ import android.os.Bundle;
 
 import com.example.ezhaby.Helper.Classes.Home.Adapter.SortAdapter;
 import com.example.ezhaby.Helper.Classes.Home.Adapter.SortHelperClass;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 public class FoodcourtActivity extends AppCompatActivity {
 
+    //Верхняя карусель сортировки
     RecyclerView sortRecycler;
     RecyclerView.Adapter adapter;
+
+    //Боковое меню
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodcourt);
 
+        //Ссылки на активити
         sortRecycler = findViewById(R.id.sortRecycler);
-        
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        //Вызов функций
         sortRecycler();
+
+
     }
 
     private void sortRecycler() {
